1163ef34f610b78ca46fc5c92cc6a67bcfa424ea
Tynchtykbek Kaldybaev <tynchtykbekkaldybaev@Tynchtykbeks-MacBook-Pro.local>
2018-06-03 22:11:27 +0900

    initialization done

--- kNN/Point.h
+++ kNN/Point.h
@@ -1,16 +1,68 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <iostream>
+using namespace std;
+ofstream fout("ex.txt");
+static int to_int(string s) {
+	int ans = 0;
+	for(int i=0; i<s.size(); i++){
+		ans = ans * 10 + s[i] - 48;
+	}
+	return ans;
+}
 class Point {
 	public:
-		//std::vector < std::pair < sting , int  > > Data;
-		std::string Class;
-		std::string Komnata;
+		vector < pair < string , int  > > Data;
+		string Class;
+		string Komnata;
 
+		void Input(string point);
 		void Output();
+
 };
-void Point::Output() {
-	std::ofstream fout("ex.txt");
-	const std::string PARSED_DATA_PATH = "/home/kadyrbek/Desktop/WiFiLocalization/parsed_data";
-	fout << PARSED_DATA_PATH;
+void Point::Input(string file_name) {
+	ifstream file(file_name);
+	if (file.fail()) {
+    	 cout << "Can't find the file " << file_name << "\n";
+    	exit(0);
+  	}
+  //	int size = 0;
+  	string line;
+  	while (getline(file, line)) {
+	    string mac_address = "";
+	    string signal = "";
+	    bool is_mac = true;
+	    for (size_t i = 0 ; i < line.length() ; ++i) {
+		      if(line[i] == ' ') continue;
+		      if (line[i] == ',') {
+		          is_mac = false;
+		          continue;
+		      }
+
+		      if (is_mac) {
+		        	mac_address += line[i];
+		      } 
+		      else {
+		        signal += line[i];
+		      }
+		}
+		Data.push_back(make_pair(mac_address, to_int(signal)));
+//		size++;
+
+	}
+
+
 }
+
+void Point::Output() {
+	
+	for(int i=0; i<Data.size(); i++)
+		fout << Data[i].first << "        " << Data[i].second << endl;
+	fout << "_______________________\n";
+}
+
+
+
+
+
--- kNN/kNN.cpp
+++ kNN/kNN.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 #include "Point.h"
-
+#include <vector>
 using namespace std;
+
+const string PARSED_DATA_PATH = "/Users/tynchtykbekkaldybaev/Desktop/WiFiLocalization/parsed_data/t203/";
+vector < Point > parsed_points;
+
+
+void initialize_data_points(string class_num, string komnata, int point_per_class){
+	for(int i = 1; i <= point_per_class; i++) {
+		string point_name = PARSED_DATA_PATH  + class_num + "."  + to_string(i);
+		Point sample;
+		sample.Input(point_name);
+		sample.Class = class_num;
+		sample.Komnata = komnata;	
+		parsed_points.push_back(sample);
+//		sample.Output();
+	}
+
+
+
+}
+
+
+void initialize_data(int number_of_classes,int point_per_class, string komnata){
+	for(int cl = 1; cl <= number_of_classes; cl++) {
+		string class_num = to_string(cl);
+		initialize_data_points(class_num, komnata, point_per_class);
+	}
+
+}
 int main(){
-	Point p;
-	p.Output();
+	int number_of_classes = 1;
+	int number_of_point_for_each_class = 5;
+	//int number_of_points = number_of_classes * number_of_point_for_each_class;
+	string komnata = "t203";
+
+	initialize_data(number_of_classes, number_of_point_for_each_class , komnata);
+
+	for(int i=0; i<parsed_points.size(); i++) {
+		parsed_points[i].Output();
+	}
+
 }
